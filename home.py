ebfffdd99079abe398574a34cf573ff8f26c680e
Julio Vieira <138531056+VieiraJulio@users.noreply.github.com>
2025-02-28 18:49:21 -0300

    06.02 Home Streamlib (criando funções para iniciar o arzmt de dados em cachê fim)

--- home.py
+++ home.py
@@ -6,15 +6,22 @@ import streamlit as st
 from joblib import load
 from notebooks.src.config import DADOS_GEO_MEDIAN, DADOS_LIMPOS, MODELO_FINAL
 
-
+@st.cache_data
 def carregar_dados_limpos():
     return pd.read_parquet(DADOS_LIMPOS)
-
+    
+@st.cache_data
 def carregar_dados_geo():
     return gpd.read_parquet(DADOS_GEO_MEDIAN)
-
+    
+@st.cache_resource
 def carregar_modelo():
     return load(MODELO_FINAL)
+
+df = carregar_dados_limpos()
+gdf_geo = carregar_dados_geo()
+modelo = carregar_modelo()
+    
 
 st.title("Previsão de preços de imóveis")
 
